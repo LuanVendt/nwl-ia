26b9d7a67ace2259d4ce7dc52e3281fe4713cbe9
Luan Vendt <luan.vendt@bitize.com.br>
2023-09-12 16:59:11 -0300

    adicionando o prompt

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,19 @@ export function App() {
             <Separator/>
 
             <form className="space-y-6">
+            <div className="space-y-2">
+                <Label>Prompt</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione um prompt..."/>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="title">Título do Youtube</SelectItem>
+                    <SelectItem value="description">Descrição do Youtube</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
               <div className="space-y-2">
                 <Label>Modelo</Label>
                 <Select disabled defaultValue="gpt3.5">
